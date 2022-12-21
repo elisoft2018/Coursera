cbf68bc0ccbd58e857d1005351417a342076eee4
john.robles <johneroblesa@gmail.com>
2022-12-20 20:10:25 -0500

    deveploment triki game fase Final

--- Week_4/Game.py
+++ Week_4/Game.py
@@ -1,7 +1,9 @@
+import numpy
+
 board = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
 ]
 
 
@@ -17,72 +19,85 @@ def positions_format(position):
 
 def validate_position(row, column):
     if 0 <= row <= 2 and 0 <= column <= 2:
-        if board[row][column] == " ":
+        if board[row][column] == "-":
             return True
     return False
 
 
 def validateWinner(letter):
-    """Validación de los valores horizontales"""
-    if board[0] == [letter, letter, letter] or \
+    if board[0] == [letter, letter, letter] or\
             board[1] == [letter, letter, letter] or \
             board[2] == [letter, letter, letter]:
         return True
 
-    """Validación de los valores horizontales"""
-    if board[0][0] == letter and \
-            board[1][0] == letter and \
+        # compara las columnas
+
+    elif board[0][0] == letter and\
+            board[1][0] == letter and\
             board[2][0] == letter:
         return True
 
-    if board[0][1] == letter and \
+    elif board[0][1] == letter and\
             board[1][1] == letter and \
             board[2][1] == letter:
         return True
 
-    if board[0][2] == letter and \
-            board[1][2] == letter and \
+    elif board[0][2] == letter and\
+            board[1][2] == letter and\
             board[2][2] == letter:
         return True
 
-    """Validación de los valores diagonales"""
-    if board[0][0] == letter and \
-            board[1][1] == letter and \
-            board[2][1] == letter:
+        # compara las diagonales
+    elif board[0][0] == letter \
+            and board[1][1] == letter and\
+            board[2][2] == letter:
         return True
 
-    if board[0][2] == letter and \
-            board[1][1] == letter and \
+    elif board[0][2] == letter and\
+            board[1][1] == letter and\
             board[2][0] == letter:
         return True
+    return False
+
+
+def is_board_filled():
+    for row in board:
+        for item in row:
+            if item == '-':
+                return False
+    return True
 
 
 def play():
     while True:
         try:
             positions = input("Ingrese una coordenada para el tablero (fila, columna)."
-                              " O digite salir para terminar el juego : ")
+                              " O digite salir para terminar el juego: ")
             if positions == "salir":
                 break
             row, column = positions_format(positions)
             print(row, column)
             if validate_position(row, column):
-                value = input("Digita X o O: ")
+                value = input("Digita x o o: ")
                 if value == "x" or value == "o":
                     board[row][column] = value
+                    show_board()
                 else:
                     print("Los valores permitidos son únicamente x y o")
                 result = validateWinner(value)
                 if result:
                     print("Ya hay un ganador! El Jugador", value)
-
-                show_board()
+                    show_board()
+                    break
+                if is_board_filled():
+                    print("Se ha presentado un empate")
+                    show_board()
+                    break
             else:
                 print("Las coordenadas ingresadas superan el tamaño del tablero,"
                       "o para las coordenadas ingresadas ya existe valor")
-                break
         except ValueError:
-            print('Opssss Debes enviar dos valores separados por coma ')
+            print('Op-ssss Debes enviar dos valores separados por coma')
             continue
 
 
